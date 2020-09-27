b47ba89c9e9acf89a130ced01e386c535d4894e2
Qiyuan Pu <pqy7172@gmail.com>
2020-09-27 10:46:07 +0800

    update

--- src_sync.sh
+++ src_sync.sh
@@ -1,6 +1,6 @@
-while inotifywait -r -e modify,create,delete /home/pqy7172/new5; do
-    rsync -avz /home/pqy7172/new5/go1.14.1 puqiyuan@10.8.11.106:/home/puqiyuan/new5
-    rsync -avz /home/pqy7172/new5/go1.14.1 deepin@10.2.3.169:/home/deepin/pqy/golang
-    rsync -avz /home/pqy7172/new5/go1.14.1 deepin@10.20.31.145:/home/deepin/pqy/golang
-    rsync -avz /home/pqy7172/new5/go1.14.1 deepin@10.3.10.34:/home/deepin/puqiyuan/golang
+while inotifywait -r -e modify,create,delete /home/pqy7172/new5/go1.14.1/src; do
+#    rsync -avz /home/pqy7172/new5/go1.14.1 puqiyuan@10.8.11.106:/home/puqiyuan/new5
+    rsync -avz /home/pqy7172/new5/go1.14.1/src deepin@10.2.3.169:/home/deepin/pqy/golang/go1.14.1
+    rsync -avz /home/pqy7172/new5/go1.14.1/src deepin@10.20.31.145:/home/deepin/pqy/golang/go1.14.1
+    rsync -avz /home/pqy7172/new5/go1.14.1/src deepin@10.3.10.34:/home/deepin/puqiyuan/golang/go1.14.1
 done
